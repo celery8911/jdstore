d5ac1abb3c48af403623a92ab36512cc99f8858f
Celery8911 <celery8911@gmail.com>
2017-09-19 21:42:42 +0800

    fix heroku bug 02

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_mailer.default_url_options = { :host => 'mighty-gorge-53800'}
+
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
